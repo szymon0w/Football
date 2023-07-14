999f4a80ffe7847d1c738b058c62dc3f9a0097d8
szymon0w <szymfre33@gmail.com>
2023-07-14 18:13:49 +0200

    initial commit

---
+++ game_engine.py
@@ -0,0 +1,3 @@
+class GameEngine:
+    def __init__(self):
+        pass
---
+++ main.py
@@ -0,0 +1,46 @@
+import pygame
+ 
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+width = 700
+height = 500
+
+pygame.init()
+ 
+# Set the width and height of the screen [width, height]
+size = (width, height)
+screen = pygame.display.set_mode(size)
+ 
+pygame.display.set_caption("My Game")
+ 
+# Loop until the user clicks the close button.
+done = False
+ 
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+ 
+# -------- Main Program Loop -----------
+while not done:
+    # --- Main event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+ 
+    # --- Game logic should go here
+ 
+    # --- Screen-clearing code goes here
+    screen.fill(WHITE)
+ 
+    # --- Drawing code should go here
+ 
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+ 
+    # --- Limit to 60 frames per second
+    clock.tick(60)
+ 
+# Close the window and quit.
+pygame.quit()
